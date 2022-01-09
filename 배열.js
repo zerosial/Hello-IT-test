dc537049197c7a230c80738729fbe6afb337642b
zerosial <zerosial@gmail.com>
2022-01-10 04:09:32 +0900

    배열을 이해하자

--- 배열.js
+++ 배열.js
@@ -1,8 +1,6 @@
-console.log('hello world')
-
 fruits = ["apple", "banana", "coconut"];
 var i = 0;
 while (i < 3) {
-  document.write('<li>'+fruits[i]+'</li>');
+  document.write('<li></li>');
   i = i + 1;
 }
