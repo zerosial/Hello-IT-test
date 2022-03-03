da17de9c11c4bab9f569970e8b18a119cb3078df
zerosial <zerosial@gmail.com>
2022-03-04 05:51:38 +0900

    3/4

--- test.js
+++ test.js
@@ -1,8 +1,4 @@
-var result = {};
-var a;
-var arr1 = ["Jett", "Ken"];
-var arr2 = [30, 40];
-a = arr1[0];
-b = arr2[0];
-result[arr1[0]] = arr2[0];
-console.log(result);
+for (let i = 0; i < 3; i++)
+var button1 = i;
+
+console.log(button1);
