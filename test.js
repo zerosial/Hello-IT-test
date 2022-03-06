fcec8a85adaa9d98c7fc5fb28bae8a10e708fd29
zerosial <zerosial@gmail.com>
2022-03-06 23:54:41 +0900

    3/6

--- test.js
+++ test.js
@@ -1,7 +1,12 @@
-var a = 1;
-
-function foo () {
-  console.log(a);
-}
-
-foo();
+    let btnRight = document.querySelector(".fas fa-arrow-right fa-3x");
+    let btnLeft = document.querySelector(".fas fa-arrow-left fa-3x");
+    let imgAll = document.querySelectorAll(".image-box");
+    btnRight.addEventListener("click",function () {
+      for(i = 0; i < 5; i++){
+        imgAll[i].style.transform = "translateX(-463.17px)";
+      };
+    });
+    let dotItem = document.querySelectorAll(".dot-item");
+    dotItem.addEventListener("click",function () {
+      imgAll[0].style.transform = "translateX(-463.17px)";
+    });
